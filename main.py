a2570f1a8fdff55b72daff43559c42e7e0938772
Nicolas Schmidt <nschmidtg@gmail.com>
2022-05-14 20:26:16 +0200

    prob_matrix computed

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from classes.spectral_analyzer import SpectralAnalizer
 from classes.image_analyzer import ImageAnalizer
 import torch
 import torchaudio
+from PIL import Image
+import pdb
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -30,6 +33,37 @@ parser.add_argument(
     help="file name of the synthetized output audio",
     required=False,
 )
+
+def main_xilo(conf):
+    torchaudio.set_audio_backend(backend='soundfile')
+    image_path = conf['image_path']
+
+    notes = 5
+    image_a = ImageAnalizer()
+    im = image_a.open(image_path)
+    image = Image.Image.split(im)
+    # pdb.set_trace()
+    R = np.array(image[0])
+    G = np.array(image[1])
+    B = np.array(image[2])
+    Grey = 0.299 * R + 0.587 * G + 0.114 * B
+    W, H = Grey.shape
+    #Grey=np.ones((H,W))
+    delta_x = int(W/notes)
+    delta_y = int(H/128)  # when using 2 synth
+    # delta_vel = int(H/128)
+
+    prob_matrix = np.zeros(notes * 128)
+    col_count = 0
+    row_count = 0
+
+    current = 0
+    for col_count in range(0, notes):
+        for row_count in range(0, 128):
+            prob_matrix[current] = np.sum(Grey[col_count * delta_x :(col_count + 1) * delta_x, row_count * delta_y :(row_count + 1) * delta_y])
+            current += 1
+
+    pdb.set_trace()
 
 
 def main(conf):
@@ -66,4 +100,5 @@ def main(conf):
 if __name__ == "__main__":
     args = parser.parse_args()
     arg_dic = dict(vars(args))
-    main(arg_dic)
+    #main(arg_dic)
+    main_xilo(arg_dic)
