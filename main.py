0b265f66c5450ba00c958021704fd0de0f86b72a
Nicolas Schmidt <nschmidtg@gmail.com>
2022-06-17 00:07:37 +0200

    lluvia strea midi

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import argparse
-from classes.spectral_analyzer import SpectralAnalizer
 from classes.image_analyzer import ImageAnalizer
-import torch
-import torchaudio
 from PIL import Image
-import pdb
 import numpy as np
-from midiutil import MIDIFile
+import mido
+from mido import Message
+import cv2
+import threading
+import time
+
+
+speed_corrector = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -14,51 +17,78 @@ parser.add_argument(
     help="path to the image to be processed",
     required=True,
 )
-parser.add_argument(
-    "--sample_rate",
-    default=16000,
-    help="sample rate of the output audio",
-    required=False,
-    type=int
-)
-parser.add_argument(
-    "--hop_size",
-    default=512,
-    help="hop size to synthetized audio. The higher, the longer",
-    required=False,
-    type=int
-)
-parser.add_argument(
-    "--out_name",
-    default='output.wav',
-    help="file name of the synthetized output audio",
-    required=False,
-)
+
+
+def send_note(outport, note, duration, vel):
+    msg = Message('note_on', note=note, velocity=vel)
+    outport.send(msg)
+    time.sleep(duration/1000)
+    msg = Message('note_off', note=note)
+    outport.send(msg)
+
+
+def global_var():
+    global speed_corrector
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():  # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        speed_corrector = np.average(frame)
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
 
 def main_xilo(conf):
-    torchaudio.set_audio_backend(backend='soundfile')
     image_path = conf['image_path']
 
-    notes = [38,40,42,43,45,47,49,50,62,64,66,67,69,71,73,74,76,78,79,81,83,85,86] 
-    track    = 0
-    channel  = 0
-    current_time     = 1   # In beats
-    tempo    = 880  # In BPM
-    quarter_length = int(1000/(tempo/60))
+    notes = [
+        38,
+        40,
+        42,
+        43,
+        45,
+        47,
+        49,
+        50,
+        62,
+        64,
+        66,
+        67,
+        69,
+        71,
+        73,
+        74,
+        76,
+        78,
+        79,
+        81,
+        83,
+        85,
+        86
+    ]
+    current_time = 1   # In beats
     n_notes = len(notes)
     image_a = ImageAnalizer()
     im = image_a.open(image_path)
     image = Image.Image.split(im)
-    # pdb.set_trace()
     R = np.array(image[0])
     G = np.array(image[1])
     B = np.array(image[2])
     Grey = 0.299 * R + 0.587 * G + 0.114 * B
     W, H = Grey.shape
-    #Grey=np.ones((H,W))
     delta_x = int(W/n_notes)
     delta_y = int(H/128)  # when using 2 synth
-    # delta_vel = int(H/128)
 
     prob_matrix = np.zeros(n_notes * 128)
     notes_matrix = [None] * (n_notes * 128)
@@ -69,71 +99,43 @@ def main_xilo(conf):
     for col_count in range(0, n_notes):
         for row_count in range(0, 128):
             notes_matrix[current] = "%s-%s" % (col_count, row_count)
-            prob_matrix[current] = np.sum(Grey[col_count * delta_x :(col_count + 1) * delta_x, row_count * delta_y :(row_count + 1) * delta_y])
+            prob_matrix[current] = np.sum(Grey[
+                col_count * delta_x:(col_count + 1) * delta_x,
+                row_count * delta_y:(row_count + 1) * delta_y
+            ])
             current += 1
 
     max_value = np.sum(prob_matrix)
     norm_probs = prob_matrix / max_value
 
-    
-    MyMIDI = MIDIFile(1, eventtime_is_ticks=False) # One track, defaults to format 1 (tempo track
-                        # automatically created)
-    MyMIDI.addTempo(track, current_time, tempo)
+    outport = mido.open_output()
 
-    for i in range(1000):
-        # pdb.set_trace()
+    y = threading.Thread(target=global_var)
+    y.start()
+
+    while(True):
         note_vel = np.random.choice(notes_matrix, p=norm_probs)
         pitch, volume = note_vel.split('-')
-        pitch = notes[int(pitch)]
+        pitch = int(notes[int(pitch)] + ((speed_corrector/255)*2-1)*0)
         volume = int(volume)
-        time_sampled = max(quarter_length, int(np.random.normal(loc=quarter_length, scale=500)))
-        # pdb.set_trace()
-        duration = 16
-        time_q = ms_to_quarters(time_sampled, quarter_length)
-        
-        MyMIDI.addNote(track, channel, pitch, current_time, 64, volume)
-        current_time = current_time + time_q
-
-    with open("major-scale.mid", "wb") as output_file:
-        MyMIDI.writeFile(output_file)
-
-def ms_to_quarters(onset, quarter_length):
-    return int(onset/quarter_length)
-
-
-def main(conf):
-    torchaudio.set_audio_backend(backend='soundfile')
-    image_path = conf['image_path']
-    sample_rate = conf['sample_rate']
-    hop_size = conf['hop_size']
-    out_name = conf['out_name']
-
-    image_a = ImageAnalizer()
-    im = image_a.open(image_path)
-    im, n_frames, n_bins = image_a.get_frames_and_bins(im)
-    R, G, B, Grey = image_a.split_channels(im)
-
-    spectral_a = SpectralAnalizer(n_frames, n_bins, hop_size, sample_rate)
-    spectral_a.plot_spectrogram(Grey)
-
-    # analyze the sources
-    R = spectral_a.stftSynth(R, R)
-    G = spectral_a.stftSynth(G, G)
-    B = spectral_a.stftSynth(B, B)
-    Grey = spectral_a.stftSynth(Grey, Grey)
-
-    # mix the sources (stereo)
-    # x = np.array([Grey - R/4, Grey - B])
-    x = Grey.to(torch.float32)
-    max_value = torch.max(torch.abs(x))
-    x /= max_value
-
-    # save audio
-    torchaudio.save(out_name, x, sample_rate=sample_rate)
+        time_sampled = max(0, np.random.normal(
+            loc=int(2000+1000*((speed_corrector/255)*2-1)),
+            scale=500
+        ))
+        x = threading.Thread(
+            target=send_note,
+            args=(outport, pitch, time_sampled, volume)
+        )
+        x.start()
+        time_sampled = max(0, np.random.normal(
+            loc=int(500+250*((speed_corrector/255)*2-1)),
+            scale=250
+        ))
+        time.sleep(time_sampled/1000)
+        current_time = current_time + time_sampled
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     arg_dic = dict(vars(args))
-    #main(arg_dic)
     main_xilo(arg_dic)
