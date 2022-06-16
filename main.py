028f77e108d4f55ad3627fd5651a8fb6e27a3861
Nicolas Schmidt <nschmidtg@gmail.com>
2022-06-16 22:24:36 +0200

    working nicely

--- main.py
+++ main.py
@@ -39,7 +39,13 @@ def main_xilo(conf):
     torchaudio.set_audio_backend(backend='soundfile')
     image_path = conf['image_path']
 
-    notes = 5
+    notes = [38,40,42,43,45,47,49,50,62,64,66,67,69,71,73,74,76,78,79,81,83,85,86] 
+    track    = 0
+    channel  = 0
+    current_time     = 1   # In beats
+    tempo    = 880  # In BPM
+    quarter_length = int(1000/(tempo/60))
+    n_notes = len(notes)
     image_a = ImageAnalizer()
     im = image_a.open(image_path)
     image = Image.Image.split(im)
@@ -50,17 +56,17 @@ def main_xilo(conf):
     Grey = 0.299 * R + 0.587 * G + 0.114 * B
     W, H = Grey.shape
     #Grey=np.ones((H,W))
-    delta_x = int(W/notes)
+    delta_x = int(W/n_notes)
     delta_y = int(H/128)  # when using 2 synth
     # delta_vel = int(H/128)
 
-    prob_matrix = np.zeros(notes * 128)
-    notes_matrix = [None] * (notes * 128)
+    prob_matrix = np.zeros(n_notes * 128)
+    notes_matrix = [None] * (n_notes * 128)
     col_count = 0
     row_count = 0
 
     current = 0
-    for col_count in range(0, notes):
+    for col_count in range(0, n_notes):
         for row_count in range(0, 128):
             notes_matrix[current] = "%s-%s" % (col_count, row_count)
             prob_matrix[current] = np.sum(Grey[col_count * delta_x :(col_count + 1) * delta_x, row_count * delta_y :(row_count + 1) * delta_y])
@@ -69,28 +75,30 @@ def main_xilo(conf):
     max_value = np.sum(prob_matrix)
     norm_probs = prob_matrix / max_value
 
-    np.random.choice(notes_matrix, p=norm_probs)
-    time_dist = np.random.normal(loc=32, scale=15)
-
-    degrees  = [60, 64, 67, 69, 72] # MIDI note number
-    track    = 0
-    channel  = 0
-    time     = 0   # In beats
-    duration = 1   # In beats
-    tempo    = 60  # In BPM
-    volume   = 100 # 0-127, as per the MIDI standard
-
-    MyMIDI = MIDIFile(1) # One track, defaults to format 1 (tempo track
+    
+    MyMIDI = MIDIFile(1, eventtime_is_ticks=False) # One track, defaults to format 1 (tempo track
                         # automatically created)
-    MyMIDI.addTempo(track,time, tempo)
+    MyMIDI.addTempo(track, current_time, tempo)
 
-    for pitch in degrees:
-        MyMIDI.addNote(track, channel, pitch, time, duration, volume)
-        time = time + 1
+    for i in range(1000):
+        # pdb.set_trace()
+        note_vel = np.random.choice(notes_matrix, p=norm_probs)
+        pitch, volume = note_vel.split('-')
+        pitch = notes[int(pitch)]
+        volume = int(volume)
+        time_sampled = max(quarter_length, int(np.random.normal(loc=quarter_length, scale=500)))
+        # pdb.set_trace()
+        duration = 16
+        time_q = ms_to_quarters(time_sampled, quarter_length)
+        
+        MyMIDI.addNote(track, channel, pitch, current_time, 64, volume)
+        current_time = current_time + time_q
 
     with open("major-scale.mid", "wb") as output_file:
         MyMIDI.writeFile(output_file)
 
+def ms_to_quarters(onset, quarter_length):
+    return int(onset/quarter_length)
 
 
 def main(conf):
